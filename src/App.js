99601d00dd57c1d25440acb52db97b78b52baccc
Romenick Tester <romenick.tester@gmail.com>
2021-02-17 19:20:46 +0000

    basic page layout structure | router routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {
+  HomePage,
+  AboutPage,
+  CartPage,
+  ProductsPage,
+  SingleProductPage,
+  CheckoutPage,
+  ErrorPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/product" component={SingleProductPage} />
+        <Route path="/checkout" component={CheckoutPage} />
+        <Route path="*" component={ErrorPage} />
+      </Switch>
+      <Footer/>
+    </Router>
+  );
 }
 
 export default App
