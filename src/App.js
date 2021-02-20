0c0701c3ab28ce6d772f0f026fa6fac438fb7b92
Romenick Tester <romenick.tester@gmail.com>
2021-02-20 09:58:36 +0000

    error page | 'exact' is added on the route

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
       <Sidebar/>
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/cart" component={CartPage} />
-        <Route path="/products" component={ProductsPage} />
-        <Route path="/product" component={SingleProductPage} />
-        <Route path="/checkout" component={CheckoutPage} />
+        <Route path="/about" exact component={AboutPage} />
+        <Route path="/cart" exact component={CartPage} />
+        <Route path="/products" exact component={ProductsPage} />
+        <Route path="/product" exact component={SingleProductPage} />
+        <Route path="/checkout" exact component={CheckoutPage} />
         <Route path="*" component={ErrorPage} />
       </Switch>
       <Footer/>
